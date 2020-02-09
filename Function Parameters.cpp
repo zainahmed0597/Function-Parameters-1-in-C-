257c56b53a18f9ea28b29029ab30ab42992480a2
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2020-02-09 14:53:50 +0500

    First commit

---
+++ Function Parameters.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printSomething( int x ){
+	cout << x << endl;
+}
+
+int main(){
+	
+	printSomething(42);
+	
+	cout << "Bla Bla Bla" << endl;
+}
+
+// Output 42
